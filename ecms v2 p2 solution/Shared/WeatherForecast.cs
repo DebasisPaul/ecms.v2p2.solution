4710b6dacff8112b896ced0344c2910cbd73e6ee
DEBASIS PAUL®™ (Zhýralý Pāvils) #BLACKLIVESSTILLMATTER #F1 #DOTNET #MSFT #BLM <DebasisPaul@users.noreply.github.com>
2022-12-05 06:54:38 -0800

    Add project files.

---
+++ ecms v2 p2 solution/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ecms_v2_p2_solution.Shared
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
